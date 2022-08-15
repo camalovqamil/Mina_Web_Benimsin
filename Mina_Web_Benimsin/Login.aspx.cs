6e25aa5ad90d0692bae27dc8ae17ba1cfd66e7d6
CQ <camalovqamil@gmail.com>
2022-08-15 12:18:11 +0400

    Login Page Desigen

--- Mina_Web_Benimsin/Login.aspx.cs
+++ Mina_Web_Benimsin/Login.aspx.cs
@@ -11,7 +11,31 @@ namespace Mina_Web_Benimsin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                if (Request.Cookies["UserName"] != null && Request.Cookies["Password"] != null)
+                {
+                    txtUserName.Text = Request.Cookies["UserName"].Value;
+                    txtPassword.Attributes["value"] = Request.Cookies["Password"].Value;
+                }
+            }
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (chkRememberMe.Checked)
+            {
+                Response.Cookies["UserName"].Expires = DateTime.Now.AddDays(30);
+                Response.Cookies["Password"].Expires = DateTime.Now.AddDays(30);
+            }
+            else
+            {
+                Response.Cookies["UserName"].Expires = DateTime.Now.AddDays(-1);
+                Response.Cookies["Password"].Expires = DateTime.Now.AddDays(-1);
+            }
+
+            Response.Cookies["UserName"].Value = txtUserName.Text.Trim();
+            Response.Cookies["Password"].Value = txtPassword.Text.Trim();
         }
     }
 }
